42ab710647886926215c1c7a1bf573cc9b1e0c80
meower here <58789337+opsonc@users.noreply.github.com>
2023-08-16 09:39:34 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta4xlwifi-user
-add_lunch_combo omni_gta4xlwifi-userdebug
-add_lunch_combo omni_gta4xlwifi-eng
+add_lunch_combo twrp_gta4xlwifi-user
+add_lunch_combo twrp_gta4xlwifi-userdebug
+add_lunch_combo twrp_gta4xlwifi-eng
